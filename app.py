d172dd6e5b05bf1f29a96d53982bdf5d620a512c
Daniyal Ahmed <daniyalahmad302003@gmail.com>
2026-02-24 11:53:05 +0500

    Fix greeting message in hello route

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ r = redis.Redis(host=redis_host, port=redis_port)
 @app.route("/")
 def hello():
     count = r.incr("visits")
-    return f"Hello from Zynex Devops Trainees! Workfloww doneee!!👋 Visits: {count}"
+    return f"Hello from Zynex Devops Trainees 👋 Visits: {count}"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
